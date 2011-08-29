152d965128d7525f678f4946ce5abad5ae5b45e9
Justin <justin.locsei@oberlin.edu>
2011-08-28 22:56:19 -0400

    An Ajax error is now treated as a standard HTTP error.

--- pressgang/utils/ajax.py
+++ pressgang/utils/ajax.py
@@ -15,16 +15,11 @@ def ajax_view(view_function):
 	def wrapper(request, *args, **kwargs):
 
 		#  Get our view response and handle exceptions if we're debugging
-		Response = HttpResponse
-		view_success = True
-		error_message = u""
 		try:
 			response_data = view_function(request, *args, **kwargs)
 		except AjaxError, e:
-			view_success = False
 			error_message = force_unicode(e.message)
-			response_data = {'message': error_message.capitalize()}
-			Response = HttpResponseBadRequest
+			return HttpResponseBadRequest(error_message.capitalize())
 		except Exception, e:
 			raise e
 
@@ -35,8 +30,7 @@ def ajax_view(view_function):
 		#  Or make it a JSON response, adding in our predetermined success value
 		#  and a possible error text obtained from a caught AjaxError
 		else:
-			response_data['success'] = view_success
-			json_response = Response(mimetype="application/json")
+			json_response = HttpResponse(mimetype="application/json")
 			json.dump(response_data, fp=json_response)
 			return json_response
 

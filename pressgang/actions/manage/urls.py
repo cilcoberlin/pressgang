cb6ee11e2e575b5cef611e80473d8b08bec77886
Justin <justin.locsei@oberlin.edu>
2011-09-02 15:44:53 -0400

    Blog syncing can now be done, and must only be done, through the browser via the management page.

--- pressgang/actions/manage/urls.py
+++ pressgang/actions/manage/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('pressgang.actions.manage.views',
 	url(r'^/?$', 'list_blogs', name="list-blogs"),
+	url(r'^sync-blogs/$', 'sync_blogs', name="sync-blogs"),
 )
--- pressgang/actions/manage/views.py
+++ pressgang/actions/manage/views.py
@@ -1,4 +1,7 @@
 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 from pressgang.accounts.decorators import can_view_blogs
 from pressgang.core.models import Blog, WordPressVersion
 from pressgang.utils.pages import Page
@@ -6,9 +9,26 @@ from pressgang.utils.pages import Page
 @can_view_blogs
 def list_blogs(request):
 	"""Displays all blogs on the site to a user."""
+
+	# Get our sync result if we have one
+	try:
+		synced = request.session['synced']
+	except KeyError:
+		synced = False
+	else:
+		del request.session['synced']
+
 	page = Page(request)
 	page.add_render_args({
 		'blogs': Blog.objects.list_by_path(),
-		'current_version': WordPressVersion.objects.current_version()
+		'current_version': WordPressVersion.objects.current_version(),
+		'synced': synced
 	})
 	return page.render('pressgang/manage/list.html')
+
+@can_view_blogs
+def sync_blogs(request):
+	"""Syncs the list of blogs installed on the server with the database."""
+	Blog.objects.sync_blogs()
+	request.session['synced'] = True
+	return HttpResponseRedirect(reverse("pressgang:list-blogs"))
